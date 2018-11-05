d35b30a3ce77fcaec11973f7e631370e2d63e382
CyprienBosserelle <cyprien.bosserelle@gmail.com>
2018-11-05 22:06:18 +1300

    Code cleanup moved to 2 new files
    Warning need to update the makefile!

--- Header.cuh
+++ Header.cuh
@@ -488,5 +488,6 @@ void SaveParamtolog(Param XParam);
 double interptime(double next, double prev, double timenext, double time);
 double BilinearInterpolation(double q11, double q12, double q21, double q22, double x1, double x2, double y1, double y2, double x, double y);
 
+
 // End of global definition
 #endif
