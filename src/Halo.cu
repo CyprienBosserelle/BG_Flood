3184234781e6a3f0d437091db0dfde542c71c958
Cyprien <cyprien.bosserelle@gmail.com>
2020-07-24 22:11:12 +1200

    starting halo filling functions

---
+++ src/Halo.cu
@@ -0,0 +1,22 @@
+﻿#include "Halo.h"
+
+template <class T> void fillHalo()
+{
+	
+}
+
+template <class T> void fillLeft(Param XParam,int ib,int leftib,int levelib,int levelleftib,T*z)
+{
+	if (ib == leftib)//
+	{
+		for (int j = 0; j < XParam.blkwidth; j++)
+		{
+			//
+			int read, write;
+			read = 1 + (j + XParam.halowidth) * XParam.halowidth + ib * XParam.blksize;
+			write = 0 + (j + halowidth) * blkmemwidth + ib * blksize;
+			z[write] = z[read];
+		}
+	}
+
+}
---
+++ src/Halo.h
@@ -0,0 +1,15 @@
+
+#ifndef HALO_H
+#define HALO_H
+
+#include "General.h"
+#include "Param.h"
+#include "Write_txt.h"
+#include "Util_CPU.h"
+#include "Arrays.h"
+#include "Mesh.h"
+
+
+
+// End of global definition
+#endif
