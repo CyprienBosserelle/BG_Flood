0c83aa634626ce66fba59ec042e78241e49c65e6
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2021-08-13 16:04:58 +1200

    Fix BUG!... almost
    l

--- src/FlowGPU.cu
+++ src/FlowGPU.cu
@@ -59,7 +59,7 @@ template <class T> void FlowGPU(Param XParam, Loop<T>& XLoop, Forcing<float> XFo
 
 	//============================================
 	// Fill Halo for flux from fine to coarse
-	//fillHaloGPU(XParam, XModel.blocks, XModel.flux);
+	fillHaloGPU(XParam, XModel.blocks, XModel.flux);
 
 	//============================================
 	// Reduce minimum timestep
@@ -123,7 +123,7 @@ template <class T> void FlowGPU(Param XParam, Loop<T>& XLoop, Forcing<float> XFo
 
 	//============================================
 	// Fill Halo for flux from fine to coarse
-	//fillHaloGPU(XParam, XModel.blocks, XModel.flux);
+	fillHaloGPU(XParam, XModel.blocks, XModel.flux);
 
 	//============================================
 	// Update advection terms (dh dhu dhv) 
