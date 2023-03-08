b64416983a6307d73c46df34d89a386b06fff000
Alice Harang <alice.harang@niwa.co.nz>
2023-03-08 13:08:21 +1300

    Add dimension river forcing

--- src/Forcing.h
+++ src/Forcing.h
@@ -186,7 +186,7 @@ struct Forcing
 	
 
 	std::vector<River> rivers;
-	/*The river is added as a vertical discharge on a chosen area (the user input consists in a time serie and a rectangular area definition).
+	/*The river is added as a vertical discharge (m3/s) on a chosen area (the user input consists in a time serie and a rectangular area definition).
 	The whole cells containing the corners of the area will be included in the area, no horizontal velocity is applied.
 	To add multiple rivers, just add different lines in the input file (one by river).
 	Ex: river = Votualevu_R.txt,1867430,1867455,3914065,3914090;
