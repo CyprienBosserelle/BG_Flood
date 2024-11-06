6e712a78e641844e00de1059a0a6aec30845bd4b
Cyprien <cyprien.bosserelle@gmail.com>
2024-11-06 21:50:52 +1300

    Update FlowMLGPU.cu

--- src/FlowMLGPU.cu
+++ src/FlowMLGPU.cu
@@ -2,7 +2,10 @@
 
 template <class T> void FlowMLGPU(Param XParam, Loop<T>& XLoop, Forcing<float> XForcing, ModelML<T> XModel)
 {
-	// Recalculate zs
+	
+	//============================================
+	//  Fill the halo for gradient reconstruction & Recalculate zs
+	fillHaloGPU(XParam, XModel.blocks, XModel.evolv, XModel.zb);
 
 	// Set max timestep
 
