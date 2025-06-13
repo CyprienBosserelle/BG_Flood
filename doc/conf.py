5a252f1f0f03736b89b0e05d395cbaeccf8f7858
Alice Harang <Alice.Harang@niwa.co.nz>
2025-06-13 14:49:40 +1200

    Test new version using Sphinx

---
+++ doc/conf.py
@@ -0,0 +1,11 @@
+extensions = [
+    'breathe',
+    'myst_parser',
+]
+
+breathe_projects = {
+    "MyProject": "../xml"
+}
+breathe_default_project = "MyProject"
+
+html_theme = "furo"  # Or "sphinx_rtd_theme" or others
