39ea8b7e6a1d75037708f73e62e06cfa921c9f6a
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2026-07-27 21:25:08 +1200

    Update Multilayer.cu

--- src/Multilayer.cu
+++ src/Multilayer.cu
@@ -428,7 +428,7 @@ template <class T> __global__ void AdvecFluxML(Param XParam, BlockP<T> XBlock,T 
 		}
 		*/
 		iub = memloc(halowidth, blkmemwidth, ix + ixshft, iy - 1, ib);
-		iut = memloc(halowidth, blkmemwidth, ix + ixshft, max(iy + 1 , XParam.XParam.blkwidth), ib);
+		iut = memloc(halowidth, blkmemwidth, ix + ixshft, min(iy + 1 , XParam.XParam.blkwidth), ib);
 
 
 		int iv = memloc(halowidth, blkmemwidth, ix, iy + iyshft, ib);
@@ -453,7 +453,7 @@ template <class T> __global__ void AdvecFluxML(Param XParam, BlockP<T> XBlock,T 
 			ivl = memloc(halowidth, blkmemwidth, ix - 1, iy+iyshft, ib);
 		}
 		*/
-		ivr = memloc(halowidth, blkmemwidth, max(ix + 1 , XParam.XParam.blkwidth), iy + iyshft, ib);
+		ivr = memloc(halowidth, blkmemwidth, min(ix + 1 , XParam.blkwidth), iy + iyshft, ib);
 		ivl = memloc(halowidth, blkmemwidth, ix - 1, iy + iyshft, ib);
 
 		
