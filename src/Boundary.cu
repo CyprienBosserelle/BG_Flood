413f534c9f8e08f6af4593d03982a3588dd00036
Cyprien Bosserelle <cyprien.bosserelle@gmail.com>
2026-07-30 14:05:28 +1200

    Update Boundary.cu

--- src/Boundary.cu
+++ src/Boundary.cu
@@ -1138,7 +1138,7 @@ template <class T> __global__ void bndFluxGPUSideEv(Param XParam, bndsegmentside
 	h[i] = max(zsX - (zsinside - hinside), T(0.0));
 
 
-	un[i] = F * XParam.Theta_H;
+	un[i] = F * XParam.theta_H;
 	//utinside = ut[inside];
 
 	
